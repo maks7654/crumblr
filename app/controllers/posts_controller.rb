45f3c9b4d997538a4aed57996be67c8512d89ba0
maks7654 <marilyncr5@aol.com>
2016-08-10 07:10:32 -0500

    Style the Post scaffold to use Bootstrap

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,10 +29,10 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
+        #format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+        #format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
   end
